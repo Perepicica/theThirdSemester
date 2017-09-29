50ba61cf07d0306e7c996324f051d35050fb29f8
Gangsta <sashika25@mail.ru>
2017-09-30 00:06:46 +0300

    почти всё переделала

--- src/practice/NewMax.java
+++ src/practice/NewMax.java
@@ -1,56 +1,39 @@
 package practice;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class NewMax {
-    static ArrayList<Integer> maxSubMass(int a[]) {
-        ArrayList arr1 = new ArrayList();
+    static List<Integer> maxSubMass(List<Integer> list) {
+        List arr1 = new ArrayList();
         int sum = 0;
-        int ind1 = 0;
-        int ind2 = 0;
+        int begin = 0;
+        int end = 0;
         int max = 0;
         int indd = 0;
-        if (a.length == 0) return arr1;
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] > 0) sum += a[i];
-            if (a[i] < 0) {
-                if (i == a.length) {
-                    if (a[i] > 0) {
-                        sum += a[i];
-                    }
-                    if (sum > max) {
-                        max = sum;
-                        ind1 = indd;
-                        ind2 = i;
-                    }
-                } else {
-                    if (sum > max) {
-                        max = sum;
-                        ind1 = indd;
-                        ind2 = i - 1;
-                    }
-
-                    if (Math.abs(a[i]) > sum) {
-                        sum = 0;
-                        indd = i + 1;
-
-                    } else {
-                        sum += a[i];
-
-                    }
+        if (list.size() == 0) return arr1;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > 0) {
+                sum += list.get(i);
+                if (i == list.size()-1 && sum > max) {
+                    begin=indd;
+                    end = i;
                 }
             }
-        }
-        if (a[a.length - 1] > 0) {
-            if (sum > max) {
-                max = sum;
-                ind1 = indd;
-                ind2 = a.length - 1;
+            if (list.get(i) < 0) {
+                if (sum > max) {
+                    max = sum;
+                    begin = indd;
+                    end = i - 1;
+                }
+                if (Math.abs(list.get(i)) > sum) {
+                    sum = 0;
+                    if(i!=list.size()-1) indd = i + 1;
+                } else sum += list.get(i);
             }
         }
-        for (int j = ind1; j <= ind2; j++) {
-            arr1.add(a[j]);
-        }
+        arr1 = list.subList(begin, end+1);
         System.out.println(arr1);
-        return arr1;
+        return arr1 ;
     }
 }
--- src/practice/Tests.java
+++ src/practice/Tests.java
@@ -2,51 +2,52 @@ package practice;
 
 import org.junit.Test;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
 public class Tests {
-    int a[] = {1, 4, 78, 54, -100, 39, 84, 92, 12};
-    List<Integer> result = Arrays.asList(39, 84, 92, 12);
+    List<Integer> a = Arrays.asList(1, 4, 78, 54, -100, 39, 84, 92, 12);
+    List<Integer> result = Arrays.asList(1, 4, 78, 54, -100, 39, 84, 92, 12);
 
-    int a1[] = {1, 4, 24, 54, 100, 39, 84, 92, 12};
+    List<Integer> a1 = Arrays.asList(1, 4, 24, 54, 100, 39, 84, 92, 12);
     List<Integer> result1 = Arrays.asList(1, 4, 24, 54, 100, 39, 84, 92, 12);
 
-    int a2[] = {9, -12, 23, 4, 0, -9};
+    List<Integer> a2 = Arrays.asList(9, -12, 23, 4, 0, -9);
     List<Integer> result2 = Arrays.asList(23, 4, 0);
 
-    int a3[] = {-9, -12, 23, 4, 0, -9, 34, 43, 0, 92, -92, 92};
+    List<Integer> a3 = Arrays.asList(-9, -12, 23, 4, 0, -9, 34, 43, 0, 92, -92, 92);
     List<Integer> result3 = Arrays.asList(23, 4, 0, -9, 34, 43, 0, 92);
 
-    int a4[] = {25,5,-1996,12,15,30};
+    List<Integer> a4 = Arrays.asList(25,5,-1996,12,15,30);
     List<Integer> result4 = Arrays.asList(12,15,30);
 
-    int a5[] = {};
+    List<Integer> a5 = Arrays.asList();
     List<Integer> result5 = Arrays.asList();
 
-    int a6[] = {-1, -2, -3, 7, -4, -5, -6};
+    List<Integer> a6 = Arrays.asList(-1, -2, -3, 7, -4, -5, -6);
     List<Integer> result6 = Arrays.asList(7);
 
-    int a7[] = {1, 2, 3, -79, 4, 5, 6, -80, 34, 4, 12, -99, 3, 1, 2};
+    List<Integer> a7 = Arrays.asList(1, 2, 3, -79, 4, 5, 6, -80, 34, 4, 12, -99, 3, 1, 2);
     List<Integer> result7 = Arrays.asList(34, 4, 12);
 
-    int a8[] = {8, -90, 76, 98, 56, 1, 2, -3, 2, 43, 91, -234, 23, 134, 22, 33, -123, 90};
+    List<Integer> a8 = Arrays.asList(8, -90, 76, 98, 56, 1, 2, -3, 2, 43, 91, -234, 23, 134, 22, 33, -123, 90);
     List<Integer> result8 = Arrays.asList(76, 98, 56, 1, 2, -3, 2, 43, 91);
 
-    int a9[] = {-1, -2, -3};
+    List<Integer> a9 = Arrays.asList(-1, -2, -3);
     List<Integer> result9 = Arrays.asList(-1);
 
     @Test
     public void maxSub() {
-       assertEquals(NewMax.maxSubMass(a),result);
-        assertEquals(NewMax.maxSubMass(a1),result1);
-        assertEquals(NewMax.maxSubMass(a2),result2);
-        assertEquals(NewMax.maxSubMass(a3),result3);
-        assertEquals(NewMax.maxSubMass(a4),result4);
-        assertEquals(NewMax.maxSubMass(a5),result5);
-        assertEquals(NewMax.maxSubMass(a6),result6);
-        assertEquals(NewMax.maxSubMass(a7),result7);
-        assertEquals(NewMax.maxSubMass(a8),result8);
-        assertEquals(NewMax.maxSubMass(a9),result9);
+        System.out.println(NewMax.maxSubMass(a).equals(result));
+        System.out.println(NewMax.maxSubMass(a1).equals(result1));
+        System.out.println(NewMax.maxSubMass(a2).equals(result2));
+        System.out.println(NewMax.maxSubMass(a3).equals(result3));
+        System.out.println(NewMax.maxSubMass(a4).equals(result4));
+        System.out.println(NewMax.maxSubMass(a5).equals(result5));
+        System.out.println(NewMax.maxSubMass(a6).equals(result6));
+        System.out.println(NewMax.maxSubMass(a7).equals(result7));
+        System.out.println(NewMax.maxSubMass(a8).equals(result8));
+        System.out.println(NewMax.maxSubMass(a9).equals(result9));
     }
 }
